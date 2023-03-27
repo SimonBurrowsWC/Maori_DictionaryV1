c2e3048c0297c51d9e65d3630029e1d1f05db1c3
SimonBurrowsWC <20396@wc.school.nz>
2023-03-27 15:12:42 +1300

    testing

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
 from flask import Flask, render_template
+import sqlite3
+from sqlite3 import Error
+
+
+DATABASE ="\dictionary.db"
 
 app = Flask(__name__)
 
 
+def create_connection(db_file):
+    try:
+        conection = sqlite3.connect(db_file)
+        return conection
+    except Error as e:
+        print(e)
+    return None
+
+
 @app.route('/')
-def hello_world():  # put application's code here
+def render_home():  # put application's code here
     return render_template("home.html")
+
+
+@app.route('/dictionary')
+def render_dictionary():  # put application's code here
+    con = create_connection(DATABASE)
+    query = "SELECT id, Maori_Word, English_Word, Definition, Level, catagory FROM products"
+    cur = con.cursor()
+    cur.execute(query)
+    product_list = cur.fetchall()
+    query = "SELECT id, name FROM category"
+    cur = con.cursor()
+    cur.execute(query)
+    word_list = cur.fetchall()
+    con.close()
+    return render_template("Dictionary.html", words=word_list)
 
 
 if __name__ == '__main__':
